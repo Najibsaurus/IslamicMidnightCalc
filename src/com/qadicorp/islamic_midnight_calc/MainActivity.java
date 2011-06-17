77e5e99d7de515a5b57dc2eb98d0a3328e38e87e
Asfand Yar Qazi <ayqazi@jumper>
2011-06-17 21:21:07 +0100

    First commit

---
+++ src/com/qadicorp/islamic_midnight_calc/MainActivity.java
@@ -0,0 +1,12 @@
+package com.qadicorp.islamic_midnight_calc;
+
+import android.os.Bundle;
+
+public class MainActivity extends android.app.Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
