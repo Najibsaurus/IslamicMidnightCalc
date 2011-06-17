70cb17fbd6357021304abee32663eee3cdd6cd7b
Asfand Yar Qazi <ayqazi@gmail.com>
2011-06-17 22:55:30 +0100

    UI basically there - functionality needed

--- gen/com/qadicorp/islamic_midnight_calc/R.java
+++ gen/com/qadicorp/islamic_midnight_calc/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int textview=0x7f050000;
+        public static final int fromTime=0x7f050000;
+        public static final int midTime=0x7f050002;
+        public static final int toTime=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/qadicorp/islamic_midnight_calc/MainActivity.java
+++ src/com/qadicorp/islamic_midnight_calc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.qadicorp.islamic_midnight_calc;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends android.app.Activity {
 
@@ -8,5 +10,14 @@ public class MainActivity extends android.app.Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        TimePicker fromTime = (TimePicker) findViewById(R.id.fromTime);
+        fromTime.setIs24HourView(Boolean.TRUE);
+
+        TimePicker toTime = (TimePicker) findViewById(R.id.toTime);
+        toTime.setIs24HourView(Boolean.TRUE);
+
+        TextView midTime = (TextView) findViewById(R.id.midTime);
+        midTime.setText("Billa");
     }
 }
