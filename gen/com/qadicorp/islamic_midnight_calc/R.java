a4f23414de6f726b022a3611ca34e25bc28d8bb6
Asfand Yar Qazi <ayqazi@gmail.com>
2011-06-24 22:57:43 +0100

    First attempt at trying make it work; why did it no work?

--- gen/com/qadicorp/islamic_midnight_calc/R.java
+++ gen/com/qadicorp/islamic_midnight_calc/R.java
@@ -15,7 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int fromTime=0x7f050000;
-        public static final int midTime=0x7f050002;
+        public static final int midTimeView=0x7f050002;
         public static final int toTime=0x7f050001;
     }
     public static final class layout {
--- src/com/qadicorp/islamic_midnight_calc/MainActivity.java
+++ src/com/qadicorp/islamic_midnight_calc/MainActivity.java
@@ -1,23 +1,43 @@
 package com.qadicorp.islamic_midnight_calc;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.TimePicker.OnTimeChangedListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-public class MainActivity extends android.app.Activity {
+public class MainActivity extends Activity implements OnTimeChangedListener {
+
+    private TimePicker fromTime, toTime;
+    private TextView midTimeView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        TimePicker fromTime = (TimePicker) findViewById(R.id.fromTime);
-        fromTime.setIs24HourView(Boolean.TRUE);
+        this.fromTime = (TimePicker) findViewById(R.id.fromTime);
+        this.fromTime.setIs24HourView(Boolean.TRUE);
+        this.fromTime.setOnTimeChangedListener(this);
 
-        TimePicker toTime = (TimePicker) findViewById(R.id.toTime);
-        toTime.setIs24HourView(Boolean.TRUE);
+        this.toTime = (TimePicker) findViewById(R.id.toTime);
+        this.toTime.setIs24HourView(Boolean.TRUE);
+        this.toTime.setOnTimeChangedListener(this);
 
-        TextView midTime = (TextView) findViewById(R.id.midTime);
-        midTime.setText("Billa");
+        this.midTimeView = (TextView) findViewById(R.id.midTimeView);
+    }
+
+    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+        GregorianCalendar from = new GregorianCalendar(1, Calendar.JANUARY, 1, this.fromTime.getCurrentHour(), this.fromTime.getCurrentMinute());
+        GregorianCalendar to = new GregorianCalendar(1, Calendar.JANUARY, 2, this.toTime.getCurrentHour(), this.toTime.getCurrentMinute());
+
+        long midPointInMillis = from.getTimeInMillis() - to.getTimeInMillis();
+
+        GregorianCalendar mid = (GregorianCalendar) from.clone();
+        mid.add(Calendar.MILLISECOND, (int) midPointInMillis);
+
+        this.midTimeView.setText("Mid point is " + mid.get(Calendar.HOUR) + ":" + mid.get(Calendar.MINUTE));
     }
 }
